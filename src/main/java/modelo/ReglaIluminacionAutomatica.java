4a897043e78bbc5a79aba90f15ece76b6428cb21
Ramón García Vizcaino <rgv0001@alu.utamed.es>
2026-05-30 11:49:18 +0200

    feat: actualizar reglas Strategy

--- src/main/java/modelo/ReglaIluminacionAutomatica.java
+++ src/main/java/modelo/ReglaIluminacionAutomatica.java
@@ -1,4 +1,12 @@
 package modelo;
 
-public class ReglaIluminacionAutomatica {
+public class ReglaIluminacionAutomatica implements Regla {
+
+    public ReglaIluminacionAutomatica() {
+    }
+
+    @Override
+    public void aplicar(Sensor[] sensores, Actuador[] actuadores) {
+        System.out.println("Aplicando regla: Iluminación Automática");
+    }
 }
