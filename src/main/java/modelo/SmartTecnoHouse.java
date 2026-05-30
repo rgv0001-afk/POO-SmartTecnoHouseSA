201618da513d23e76cacb3f4e9b556639b3fc58b
Ramón García Vizcaino <rgv0001@alu.utamed.es>
2026-05-30 11:40:05 +0200

    feat: mejora SmartTecnoHouse para soportar el patrón Strategy

--- src/main/java/modelo/SmartTecnoHouse.java
+++ src/main/java/modelo/SmartTecnoHouse.java
@@ -1,6 +1,7 @@
 package modelo;
 
-public class SmartTecnoHouse {
+public class SmartTecnoHouse
+{
 
     private Sensor[] sensores;
     private Actuador[] actuadores;
@@ -10,7 +11,8 @@ public class SmartTecnoHouse {
     private int numActuadores;
     private int numReglas;
 
-    public SmartTecnoHouse() {
+    public SmartTecnoHouse()
+    {
         sensores = new Sensor[10];
         actuadores = new Actuador[10];
         reglas = new Regla[10];
@@ -19,49 +21,57 @@ public class SmartTecnoHouse {
         numReglas = 0;
     }
 
-    public void añadirSensor(Sensor s) {
+    public void añadirSensor(Sensor s)
+    {
         if (numSensores < 10) {
             sensores[numSensores] = s;
             numSensores = numSensores + 1;
         }
     }
 
-    public void añadirActuador(Actuador a) {
+    public void añadirActuador(Actuador a)
+    {
         if (numActuadores < 10) {
             actuadores[numActuadores] = a;
             numActuadores = numActuadores + 1;
         }
     }
 
-    public void añadirRegla(Regla r) {
+    public void añadirRegla(Regla r)
+    {
         if (numReglas < 10) {
             reglas[numReglas] = r;
             numReglas = numReglas + 1;
         }
     }
 
-    public void actualizarSensores() {
+    public void actualizarSensores()
+    {
         for (int i = 0; i < numSensores; i++) {
             sensores[i].actualizarValor();
         }
     }
 
-    public void aplicarReglas() {
+    public void aplicarReglas()
+    {
         for (int i = 0; i < numReglas; i++) {
-            reglas[i].aplicar();
+            reglas[i].aplicar(sensores, actuadores);
         }
     }
 
-    public void mostrarEstado() {
+    public void mostrarEstado()
+    {
         System.out.println("=== ESTADO DE LA CASA ===");
 
         System.out.println("Sensores:");
-        for (int i = 0; i < numSensores; i++) {
+        for (int i = 0; i < numSensores; i++)
+        {
             System.out.println("- " + sensores[i].getNombre() + ": " + sensores[i].getEstadoActual());
         }
 
         System.out.println("Actuadores:");
-        for (int i = 0; i < numActuadores; i++) {
+        for (int i = 0; i < numActuadores; i++)
+        {
             System.out.println("- " + actuadores[i].getNombre() + ": " + actuadores[i].getEstadoActual());
         }
     }
